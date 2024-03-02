45717dd2c9ea806a11b1739588dfed2c8aaae511
body <am5753@fayoum.edu.eg>
2024-03-02 05:25:57 +0200

    size

--- lib/widgets/app_large_text.dart
+++ lib/widgets/app_large_text.dart
@@ -5,18 +5,15 @@ class AppLargeText extends StatelessWidget {
   final String text;
   final Color color;
   AppLargeText(
-      {Key? key, this.size = 30, required this.text, this.color = Colors.black})
+      {Key? key, this.size = 40, required this.text, this.color = Colors.black})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(
-          color: color,
-          fontSize: size,
-          fontWeight: FontWeight.bold
-      ),
+      style:
+          TextStyle(color: color, fontSize: size, fontWeight: FontWeight.bold),
     );
   }
 }
